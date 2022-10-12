3954bd5c48b0bb604b5edaf2f24030b60f81f119
hanbitguri <asjask123@gmail.com>
2022-10-12 15:02:05 +0900

    🚧 Add user-gallery Carousel

--- js/_product-carousel.js
+++ js/_product-carousel.js
@@ -6,9 +6,34 @@ const slider = tns({
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
   controls: false,
+
   navContainer: '.product-carousel-thumbnail .thumbnail-list',
   navAsThumbnails: true,
   arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: true,
+})
+
+const userGallery = tns({
+  container: '.user-gallery .slider-list',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  loop: false,
+  controls: false,
+  gutter: 4,
+  edgePadding: 16,
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      controls: true,
+      edgePadding: 32,
+    },
+  },
 })
