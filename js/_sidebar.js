a038f91cfb78e4ef174ebb114fdeb42ad2523c1c
전한빛 <asjask123@gmail.com>
2022-10-15 20:52:34 +0900

    💄 Handle : sidebar-drawer-menu toggling

--- js/_sidebar.js
+++ js/_sidebar.js
@@ -13,3 +13,15 @@ function sideBarClose() {
   overlay.classList.remove('is-active')
 }
 overlay.addEventListener('click', sideBarClose)
+
+const sideBarDrawerButtonList = document.querySelectorAll(
+  '.sidebar-nav .drawer-button'
+)
+function toggleDrawerMenu() {
+  const drawerMenu = this.parentNode
+  drawerMenu.classList.toggle('is-open')
+  drawerMenu.classList.toggle('is-active')
+}
+sideBarDrawerButtonList.forEach(function (button) {
+  button.addEventListener('click', toggleDrawerMenu)
+})
