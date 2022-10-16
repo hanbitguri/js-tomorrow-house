92231a5d69921352b4618084b07d74735760a298
전한빛 <asjask123@gmail.com>
2022-10-16 23:19:49 +0900

    💄 Handle : Order-cta bookmark-button toggle

--- js/_order-cta.js
+++ js/_order-cta.js
@@ -1,6 +1,5 @@
 const orderCta = document.querySelector('.order-cta')
 const [bookmarkButton, buyButton] = orderCta.children
-console.log(orderCta.children)
 
 const orderModal = document.querySelector('.order-form-modal')
 const orderModalOverlay = document.querySelector('.overlay')
@@ -14,3 +13,22 @@ orderModalOverlay.addEventListener('click', () => {
   orderModal.classList.remove('is-open')
   orderModalOverlay.classList.remove('is-active')
 })
+
+function toggleBookmarkButton() {
+  const [icon, count] = this.children
+  let span = Number(count.innerHTML.replaceAll(',', ''))
+  let newSpan = span
+
+  bookmarkButton.classList.toggle('is-active')
+  if (this.classList.contains('is-active')) {
+    icon.classList.replace('ic-bookmark', 'ic-bookmark-filled')
+    newSpan += 1
+  } else {
+    icon.classList.replace('ic-bookmark-filled', 'ic-bookmark')
+    newSpan -= 1
+  }
+  count.innerHTML = newSpan.toLocaleString()
+  count.setAttribute('aria-label', `북마크 ${newSpan.toLocaleString()}개`)
+}
+
+bookmarkButton.addEventListener('click', toggleBookmarkButton)
