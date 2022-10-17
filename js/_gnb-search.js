4f91e3fbee13f16cb8c91d9397d0e8bbffed3111
hanbitguri <asjask123@gmail.com>
2022-10-17 11:31:30 +0900

    💄 Handle : history delete-all-button actioning

--- js/_gnb-search.js
+++ js/_gnb-search.js
@@ -3,6 +3,9 @@ const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryContentList.children.length === 0) {
+    return
+  }
   gnbSearchHistory.classList.add('is-active')
   if (gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeSearchHistory)
@@ -16,3 +19,15 @@ function closeSearchHistory(e) {
   }
 }
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+const gnbSearchAllRemoveButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+const gnbSearchHistoryContentList = gnbSearchHistory.querySelector(
+  '.search-history-content .search-history-list '
+)
+function deleteAllHistory() {
+  gnbSearchHistoryContentList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+}
+gnbSearchAllRemoveButton.addEventListener('click', deleteAllHistory)
