1b6af30aa7fe92c4aec6dd581cccc8d097f97c65
hanbitguri <asjask123@gmail.com>
2022-10-17 11:07:41 +0900

    💄 Handle Toggling : gnb-search with optimization

---
+++ js/_gnb-search.js
@@ -0,0 +1,18 @@
+const gnbSearch = document.querySelector('.gnb-search')
+const gnbSearchInput = gnbSearch.querySelector('input')
+const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+
+function openGnbSearchHistory() {
+  gnbSearchHistory.classList.add('is-active')
+  if (gnbSearchHistory.classList.contains('is-active')) {
+    window.addEventListener('click', closeSearchHistory)
+  }
+}
+
+function closeSearchHistory(e) {
+  if (!gnbSearch.contains(e.target)) {
+    gnbSearchHistory.classList.remove('is-active')
+    window.removeEventListener('click', closeSearchHistory)
+  }
+}
+gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
