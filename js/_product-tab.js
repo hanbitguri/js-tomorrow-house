bd12d47fc5631a03d1c59b9dd29a6be12daafb4f
전한빛 <asjask123@gmail.com>
2022-10-17 21:27:43 +0900

    💄 Handle Scrolling with getBoundingClientRect()

--- js/_product-tab.js
+++ js/_product-tab.js
@@ -1,6 +1,7 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
-
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
 let currentActiveTab = productTab.querySelector('.is-active')
 
 function clickActiveTab() {
@@ -11,7 +12,21 @@ function clickActiveTab() {
     currentActiveTab = tabItem
   }
 }
+function scrollPanel() {
+  const tabItem = this.parentNode.getAttribute('aria-labelledby')
+  console.log(tabItem)
+  const tabPanel = document.querySelector(`#${tabItem}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
+}
 
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', clickActiveTab)
+  button.addEventListener('click', scrollPanel)
 })
